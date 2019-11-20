461ee7c69b7979799cedd2b9702aa0d5df5981dc
ricardo lira <r.liraf@gmail.com>
2019-11-19 22:56:32 -0300

    add functions to train set

--- chatbot_tools.py
+++ chatbot_tools.py
@@ -1,6 +1,14 @@
 """Chatbot tutorial."""
 import numpy as np
 import tensorflow as tf
+from tensorflow.contrib.rnn import BasicLSTMCell
+from tensorflow.contrib.rnn import DropoutWrapper
+from tensorflow.contrib.rnn import MultiRNNCell
+from tensorflow.contrib.sec2sec import (prepare_attention,
+                                        attention_decoder_fn_train,
+                                        dynamic_rnn_decoder)
+from tensor.nn import dropout
+from tensorflow.nn import bidirectional_dynamic_rnn
 import re
 import time
 
@@ -81,3 +89,84 @@ def proprocess_targets(targets, word2int, batch_size=10):
     rhs = tf.strided_slice(targets, [0, 0,], [batch_size, -1], [1, 1])
     preprocessed_targets = tf.concat([lhs, rhs], 1)
     return preprocessed_targets
+
+
+def encoder_rnn_layer(
+    rnn_inputs, rnn_size, num_layers, keep_prob, sequence_length
+):
+    """Create the Encoder RNN Layer."""
+    lstm = BasicLSTMCell(rnn_size)
+    lstm_dropout = DropoutWrapper(lstm, input_keep_prob=keep_prob)
+    encoder_cell = MultiRNNCell([lstm_dropout] * num_layers)
+    _, encoder_state = bidirectional_dynamic_rnn(
+        cell_fw=encoder_cell,
+        cell_bw=encoder_cell,
+        sequence_length=sequence_length,
+        inputs=rnn_inputs,
+        dtype=tf.float32
+    )
+    return encoder_state
+
+
+def decode_training_set(
+    encoder_state, decoder_cell, decoder_enbedded_input,
+    sequence_length, decoding_scope, output_function,
+    keep_prob, batch_size
+):
+    """Decode the training set."""
+    attention_states = tf.zeros([batch_size, 1, decoder_cell.output_size])
+    (attention_keys,
+     attentions_values,
+     attention_score_function,
+     attention_construct_function) = prepare_attention(
+        attention_states,
+        attention_option='bahdanau',
+        num_units=decoder_cell.output_size
+    )
+    training_decoder_function = attention_decoder_fn_train(
+        encoder_state[0],
+        attention_keys,
+        attentions_values,
+        attention_score_function,
+        attention_construct_function,
+        name='attn_dec_train'
+    )
+    decoder_output, _, _ = dynamic_rnn_decoder(
+        decoder_cell, training_decoder_function, decoder_enbedded_input,
+        sequence_length, scope=decoding_scope
+    )
+    decoder_output_dropout = dropout(decoder_output, keep_prob)
+
+    return output_function(decoder_output_dropout)
+
+# 
+# def decode_training_set(
+#     encoder_state, decoder_cell, decoder_enbedded_input,
+#     sequence_length, decoding_scope, output_function,
+#     keep_prob, batch_size
+# ):
+#     """Decode the training set."""
+#     attention_states = tf.zeros([batch_size, 1, decoder_cell.output_size])
+#     (attention_keys,
+#      attentions_values,
+#      attention_score_function,
+#      attention_construct_function) = prepare_attention(
+#         attention_states,
+#         attention_option='bahdanau',
+#         num_units=decoder_cell.output_size
+#     )
+#     training_decoder_function = attention_decoder_fn_train(
+#         encoder_state[0],
+#         attention_keys,
+#         attentions_values,
+#         attention_score_function,
+#         attention_construct_function,
+#         name='attn_dec_train'
+#     )
+#     decoder_output, _, _ = dynamic_rnn_decoder(
+#         decoder_cell, training_decoder_function, decoder_enbedded_input,
+#         sequence_length, scope=decoding_scope
+#     )
+#     decoder_output_dropout = dropout(decoder_output, keep_prob)
+#
+#     return output_function(decoder_output_dropout)
